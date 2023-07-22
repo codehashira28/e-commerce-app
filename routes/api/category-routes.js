bcec014950cb4bf4e71f956beeba821664f05a85
codehashira28 <123044813+codehashira28@users.noreply.github.com>
2023-07-22 16:13:10 -0500

    added code to return all categories with product information included to the proper GET request endpoint

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,13 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
+  const categoryData = await Category.findAll({
+    include: [Product]
+  });
   // be sure to include its associated Products
+  return res.json(categoryData);
 });
 
 router.get('/:id', (req, res) => {
